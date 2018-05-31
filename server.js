7ea371965f79e3c64125e8c5f809afa88cb12741
Jake Wymer <jakewymer@att.net>
2018-05-31 16:22:33 -0500

    add production routing

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ const bodyParser = require('body-parser');
 require('./models/Entry');
 const PORT = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
 mongoose.connect(keys.mongoURI);
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
